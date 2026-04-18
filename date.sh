fa71a9bbec6fcf61fd9b5fd1393a21e8c802eaf9
Ahmed sultan <ahmedsultan1960@gmail.com>
2026-04-18 18:59:21 +0200

    starting to impelement some now bash scripts

--- date.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-today=$(date +"%Y-%m-%d")
-backup_dir="backup-$today"
-
-echo "creating backup directory: $backup_dir"
-mkdir "$backup_dir"
-echo "Backup directory created successfully."

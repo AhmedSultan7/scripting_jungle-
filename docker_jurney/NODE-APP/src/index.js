0ddb8e530fb983a1d74c91f70664f5ea35509e1b
Ahmed sultan <ahmedsultan1960@gmail.com>
2026-04-26 20:42:13 +0300

    learnd have to use multiple docker compose files for multibel enviroments

--- docker_jurney/NODE-APP/src/index.js
+++ docker_jurney/NODE-APP/src/index.js
@@ -5,7 +5,7 @@ const PORT = 4000;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>    <p>This is a simple Express server running in a Docker container.</p> docker run -p 4000:4000 node-app'); 
+  res.send('<h1>Hello World!</h1>    <p>This is a simple Express server running in a Docker container.</p> '); 
 });
 
 app.listen(PORT, () => {
